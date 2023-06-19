230eb8d72ec64ae16db4dd7979632f8a0ada7da1
ANA BEATRIZ MARTINS <ana_beatriz-martins@estudante.sesisenai.org.br>
2023-06-19 09:57:11 -0300

    new java classes

---
+++ Collections/src/main/java/com/mycompany/collections/Aluno.java
@@ -0,0 +1,62 @@
+package com.mycompany.collections;
+
+public class Aluno extends Pessoa {
+    String matricula, turma;
+    int ano;
+
+    public Aluno(String matricula, String turma, int ano, String nome, String cpf, int idade) {
+        super(nome, cpf, idade);
+        this.matricula = matricula;
+        this.turma = turma;
+        this.ano = ano;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+}
---
+++ Collections/src/main/java/com/mycompany/collections/Professor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.collections;
+
+/**
+ *
+ * @author ana_beatriz-martins
+ */
+public class Professor {
+    
+}
